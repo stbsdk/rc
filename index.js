614ede1a6044926fa0c6f21643dff2d205ee84af
DarkPark <darkpark@pisem.net>
2015-12-28 01:32:54 +0200

    add spa-keys inheritance and rework

--- index.js
+++ index.js
@@ -18,56 +18,44 @@
 
 'use strict';
 
-/* eslint quote-props: 0 */
+var keys = require('spa-keys');
+
+
+// add STB-specific keys
+keys.back         = 8;    // Backspace
+keys.channelPrev  = 1009; // Shift+Tab
+keys.channelNext  = 9;    // Tab
+keys.ok           = 13;   // Enter
+keys.exit         = 27;   // Esc
+keys.volumeUp     = 107;  // NUMPAD +
+keys.volumeDown   = 109;  // NUMPAD -
+keys.f1           = 112;  // F1
+keys.f2           = 113;  // F2
+keys.f3           = 114;  // F3
+keys.f4           = 115;  // F4
+keys.refresh      = 116;  // F5
+keys.frame        = 117;  // F6
+keys.phone        = 119;  // F8
+keys.set          = 120;  // F9
+keys.tv           = 121;  // F10
+keys.menu         = 122;  // F11
+keys.web          = 123;  // F12
+keys.mic          = 2032;
+keys.rewind       = 2066; // Alt+B
+keys.forward      = 2070; // Alt+F
+keys.app          = 2076; // Alt+L
+keys.usbMounted   = 2080; // Alt+P
+keys.usbUnmounted = 2081; // Alt+Q
+keys.playPause    = 2082; // Alt+R
+keys.stop         = 2083; // Alt+S
+keys.power        = 2085; // Alt+U
+keys.record       = 2087; // Alt+W
+keys.info         = 2089; // Alt+Y
+keys.mute         = 2192;
+keys.clock        = 2032;
+keys.audio        = 2071; // Alt+G
+keys.keyboard     = 2076; // Alt+L
+
 
 // public
-module.exports = {
-    back         : 8,    // Backspace
-    'delete'     : 46,
-    channelPrev  : 1009, // Shift+Tab
-    channelNext  : 9,    // Tab
-    ok           : 13,   // Enter
-    exit         : 27,   // Esc
-    up           : 38,   // UP ARROW
-    down         : 40,   // DOWN ARROW
-    left         : 37,   // LEFT ARROW
-    right        : 39,   // RIGHT ARROW
-    pageUp       : 33,   // Page Up
-    pageDown     : 34,   // Page Down
-    end          : 35,
-    home         : 36,
-    volumeUp     : 107,  // NUMPAD +
-    volumeDown   : 109,  // NUMPAD -
-    f1           : 112,  // F1
-    f2           : 113,  // F2
-    f3           : 114,  // F3
-    f4           : 115,  // F4
-    refresh      : 116,  // F5
-    frame        : 117,  // F6
-    phone        : 119,  // F8
-    set          : 120,  // F9
-    tv           : 121,  // F10
-    menu         : 122,  // F11
-    web          : 123,  // F12
-    mic          : 2032,
-    rewind       : 2066, // Alt+B
-    forward      : 2070, // Alt+F
-    app          : 2076, // Alt+L
-    usbMounted   : 2080, // Alt+P
-    usbUnmounted : 2081, // Alt+Q
-    playPause    : 2082, // Alt+R
-    stop         : 2083, // Alt+S
-    power        : 2085, // Alt+U
-    record       : 2087, // Alt+W
-    info         : 2089, // Alt+Y
-    mute         : 2192,
-    clock        : 2032,
-    audio        : 2071, // Alt+G
-    keyboard     : 2076  // Alt+L
-};
-
-
-if ( DEBUG ) {
-    // expose to the global scope
-    window.keys = module.exports;
-}
+module.exports = keys;
